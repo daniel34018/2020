0aab2f42dc81f207714d007aff3833c1db1bbd29
Daniel Matheo <daniel34018@gmail.com>
2020-07-29 00:00:27 -0500

    interfaz index completa

---
+++ views/site/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'EliTech-EC-';
+?>
+
+<div class="site-index">
+
+    <div class="jumbotron">
+
+<center>
+<div class="responsive-logo">
+
+<img class="image-logo" src="../views/site/logo6.jpeg"  alt="Emblema de la organización " >
+</div></center>
+</br>
+
+        <p class="lead">Soluciones Tecnológicas que van hacia ti.</p>
+   
+        <p><a class="btn" href="http://www.yiiframework.com">Nuevas promociones!</a></p>
+    </div>
+
+
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Infraestructura Tecnológica</h2>
+
+                <p>Todo tu equipamiento tecnológico debe tener un optimo funcionamiento con el fin de mantener la continuidad del negocio o tus actividad en el hogar. Descubra como mejorar el potencial de su red, de sus equipos de cómputo y comunicaciones</p>
+
+                <p><a class="btn btn-default" href="#">Tecnología &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Asesoria y Capacitación</h2>
+
+                <p>Descubra las mejores soluciones para sus problemas y optimice procesos con la implemenación de modernos sistemas tecnológicos de hardware y software.
+                Actualice sus conocimientos y mantengase al dia en las nuevas tecnologías con nuestros planes de capacitaciones personalizados</p>
+
+                <p><a class="btn btn-default" href="#">Asesoria&Capacitación &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Desea una inspección personalizada?</h2>
+
+                <p>La mejor manera de aterrizar su solución sobre un proyecto tangible es una revisión persoanlizada del caso. Solicitelo aqui!!! </p>
+
+                <p><a class="btn btn-default" href="#">Solicitar Inspección &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
